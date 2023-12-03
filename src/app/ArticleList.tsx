b573c6a8a29989743b23d8b7104a658e73602732
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-12-03 11:14:35 +0900

    ArticleList (css)

--- src/app/ArticleList.tsx
+++ src/app/ArticleList.tsx
@@ -5,8 +5,8 @@ import React from "react";
 const ArticleList = () => {
   return (
     <div>
-      <article>
-        <Link href="#">
+      <article className="shadow my-4 flex flex-col">
+        <Link href="#" className="hover:opacity-75">
           <Image
             src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
             alt=""
@@ -14,17 +14,57 @@ const ArticleList = () => {
             height={300}
           />
         </Link>
-        <div>
-          <Link href="#">Technology</Link>
-          <Link href="#">Learning Next.js</Link>
-          <p>By Shu.Matsumoto on 2023/12/3</p>
-          <Link href="#">
+        <div className="bg-white flex flex-col justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            Technology
+          </Link>
+          <Link
+            href="#"
+            className="text-slate-900 text-3xl font-bold hover:text-gray-700"
+          >
+            Learning Next.js
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">Published on 2023/12/3</p>
+          <Link href="#" className="text-slate-900 pb-6">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque
             dolor eum perferendis officia temporibus veniam earum est,
             consequuntur nemo, unde delectus fugiat laudantium. Aliquam
             doloribus similique dicta nisi possimus est!
           </Link>
-          <Link href="#">Read More</Link>
+          <Link href="#" className="text-pink-800 hover:text-black">
+            Read More
+          </Link>
+        </div>
+      </article>
+      <article className="shadow my-4 flex flex-col">
+        <Link href="#" className="hover:opacity-75">
+          <Image
+            src="https://source.unsplash.com/collection/1346951/1000x500?sig=2"
+            alt=""
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div className="bg-white flex flex-col justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            Technology
+          </Link>
+          <Link
+            href="#"
+            className="text-slate-900 text-3xl font-bold hover:text-gray-700"
+          >
+            Learning Next.js
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">Published on 2023/12/3</p>
+          <Link href="#" className="text-slate-900 pb-6">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque
+            dolor eum perferendis officia temporibus veniam earum est,
+            consequuntur nemo, unde delectus fugiat laudantium. Aliquam
+            doloribus similique dicta nisi possimus est!
+          </Link>
+          <Link href="#" className="text-pink-800 hover:text-black">
+            Read More
+          </Link>
         </div>
       </article>
     </div>
