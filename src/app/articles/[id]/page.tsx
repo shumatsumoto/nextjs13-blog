eed4bf258330121490428c592d5537b58def7b07
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-12-10 14:04:55 +0900

    文字数制限設定

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -4,18 +4,19 @@ import React from "react";
 
 const Article = async ({ params }: { params: { id: string } }) => {
   const detailArticle = await getDetailArticle(params.id);
-  console.log(detailArticle);
   return (
     <div className="max-w-3xl mx-auto p-5">
       <Image
-        src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+        src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${detailArticle.id}`}
         alt=""
         width={1280}
         height={300}
       />
-      <h1 className="text-4xl text-center mb-10 mt-10">タイトル</h1>
+      <h1 className="text-4xl text-center mb-10 mt-10">
+        {detailArticle.title}
+      </h1>
       <div className="text-lg leading-relaxed text-justify">
-        <p>本文</p>
+        <p>{detailArticle.content}</p>
       </div>
     </div>
   );
