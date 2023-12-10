27c1b00e8560d8835ccf2de4fa44bd0edf7c78ed
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-12-10 14:11:52 +0900

    Footer調整

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -2,9 +2,7 @@ import { notFound } from "next/navigation";
 import { Article } from "./types";
 
 export const getAllArticles = async (): Promise<Article[]> => {
-  const res = await fetch(`http://localhost:3001/posts`, {
-    cache: "force-cache",
-  }); // SSR
+  const res = await fetch(`http://localhost:3001/posts`); // SSR
 
   if (!res.ok) {
     throw new Error("エラーが発生しました。");
