145877e6a8cebbbdd76a114fc0660e13558fb112
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-12-16 19:46:14 +0900

    削除ボタン作成中

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,9 +1,11 @@
-import { getDetailArticle } from "@/blogAPI";
+import DeleteButton from "@/app/components/DeleteButton";
+import { deleteArticle, getDetailArticle } from "@/blogAPI";
 import Image from "next/image";
 import React from "react";
 
 const Article = async ({ params }: { params: { id: string } }) => {
   const detailArticle = await getDetailArticle(params.id);
+  const handleDelete = async () => {};
   return (
     <div className="max-w-3xl mx-auto p-5">
       <Image
@@ -17,6 +19,9 @@ const Article = async ({ params }: { params: { id: string } }) => {
       </h1>
       <div className="text-lg leading-relaxed text-justify">
         <p>{detailArticle.content}</p>
+      </div>
+      <div className="text-right mt-3">
+        <DeleteButton id={detailArticle.id} />
       </div>
     </div>
   );
